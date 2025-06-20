2177d828e1887cd67058ae46d7feb301ceacd719
Xola Mkhatshwa <mkhatshwa.xola@gmail.com>
2025-06-20 22:30:48 +0200

    Initial commit

---
+++ src/test/java/com/digiexperia/springboot/todos/TodosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digiexperia.springboot.todos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
